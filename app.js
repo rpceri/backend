6cabab5f0075533f04b1c71e7fc87661225c8359
Raphael Pazdzior <rpazdzior@ceri-informatique.fr>
2022-02-03 23:07:05 +0100

    ajout pour loguer des données postées

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
+/**
+ * utilisation de middleware
+ * app.use(express.json()) met les données post à dispo dans req.body 
+ * */
 const express = require('express');
 
 const app = express();
+app.use(express.json());  //met les données post à dispo dans req.body 
 
-app.use((req, res) => {
+//headers permettant de lever l'erreur causées apr le CORS  « Cross Origin Resource Sharing » et donc :
+//d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+//d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
+
+  //app.usee permet de tout intercepter (het post etc..) au lieu de ce on peut utiliser app.get
+  app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+  
+/*  app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
-
+*/
 
 module.exports = app; // pour y accéder depuis note serveur node
