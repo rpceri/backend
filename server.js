365b60c9a27863cc77aea17b78f7b17e0b0fc120
Raphael Pazdzior <rpazdzior@ceri-informatique.fr>
2022-02-03 22:37:16 +0100

    adding middleware

--- server.js
+++ server.js
@@ -14,11 +14,46 @@ const app = require('./app');
     res.end('Voilaaaaa') // methode end de l'objet reponse
 });
 */
+const port = process.env.PORT || '3000';
+app.set('port', port); // on doit préciser a express sur quel port l'appli va tourner
 
-app.set('port', process.env.PORT || 3000); // on doit préciser a express sur quel port l'appli va tourner
+
+
+//a fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur ;
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
 const server = http.createServer(app);
 
+server.on('error', errorHandler);
+
+
+//un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+// fin  écouteur d'évènements
+
 //le serveur doit écouter et attendre : variable environnement : si la plateforme de déploiement propose un port par défaut
-server.listen(process.env.PORT || 3000)
+server.listen(port)
 
 
